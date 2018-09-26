b7d2b1f4ad6d57c90ade0b1ba3fdf7bdc013c625
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-09-26 16:18:59 -0300

    Add files via upload

---
+++ cabe-foto/cabe-foto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace cabe_foto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fotoX, fotoY, prX, prY;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            fotoX = int.Parse(vet[0]);
+            fotoY = int.Parse(vet[1]);
+            vet = Console.ReadLine().Split(' ');
+            prX = int.Parse(vet[0]);
+            prY = int.Parse(vet[1]);
+            if ((fotoX <= prX && fotoY <= prY) || (fotoY <= prX && fotoX <= prY))
+            {
+                Console.WriteLine("SIM");
+            }
+            else
+            {
+                Console.WriteLine("NAO");
+            }
+            Console.ReadLine();
+        }
+    }
